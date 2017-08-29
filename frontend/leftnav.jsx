cce0af6a61655af66a0853c7a7e3e3797f9fb065
Spencer Chan <schan1031@gmail.com>
2017-08-29 10:48:43 -0700

    fix photo size

--- frontend/leftnav.jsx
+++ frontend/leftnav.jsx
@@ -5,7 +5,7 @@ export default class LeftNav extends React.Component {
   render() {
     return (
       <div className='nav'>
-        <img src='./assets/portrait.JPG' height='42' width='42'/>
+        <img className='portrait' src='./assets/square.png' height='150' width='150'/>
         <h2>Spencer Chan</h2>
         <ul className='navList'>
           <li>Flocking Simulation</li>
