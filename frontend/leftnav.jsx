4f7851c1ccc1e5ab549be3a7c8e16bb320464687
Spencer Chan <schan1031@gmail.com>
2017-08-28 18:25:50 -0700

    portrait showing

--- frontend/leftnav.jsx
+++ frontend/leftnav.jsx
@@ -5,7 +5,7 @@ export default class LeftNav extends React.Component {
   render() {
     return (
       <div className='nav'>
-        <span>pic</span>
+        <img src='./assets/portrait.JPG' height='42' width='42'/>
         <h2>Spencer Chan</h2>
         <ul className='navList'>
           <li>Flocking Simulation</li>
