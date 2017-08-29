ec9ee6f55a875dc0e3a597cc8378934cd20ae14d
Spencer Chan <schan1031@gmail.com>
2017-08-29 13:48:42 -0700

    about component

---
+++ frontend/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class About extends React.Component {
+  render() {
+    return(
+      <div>
+        <span>image</span>
+        <p>About me blahblah</p>
+      </div>
+    );
+  }
+}
--- frontend/main.jsx
+++ frontend/main.jsx
@@ -4,7 +4,8 @@ import LeftNav from './leftnav';
 import { HashRouter, Route } from 'react-router-dom';
 import Flock from './flock';
 import Draft from './draft';
-
+import Scriber from './scriber';
+import About from './about';
 
 class Root extends React.Component {
   render () {
@@ -15,6 +16,8 @@ class Root extends React.Component {
             <LeftNav/>
             <Route path='/flock' component={Flock} />
             <Route path='/draft' component={Draft} />
+            <Route path='/scriber' component={Scriber} />
+            <Route path='/about' component={About} />
           </div>
         </HashRouter>
       </div>
