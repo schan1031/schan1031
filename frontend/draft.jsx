7a426af7a0fcddf51522d3b4deca3fabcab1c3b3
Spencer Chan <schan1031@gmail.com>
2017-08-31 11:33:07 -0700

    flock vid, draft image

--- frontend/draft.jsx
+++ frontend/draft.jsx
@@ -4,7 +4,10 @@ export default class DraftRoyale extends React.Component {
   render() {
     return(
       <div className='main-panel animated fadeIn'>
-        <span>image</span>
+        <h2>DraftRoyale</h2>
+        <a target='_blank' href='http://www.draftroyale.co/'>
+          <img src='./assets/DraftRoyale.png' width='600'/>
+        </a>
         <p>DraftRoyale is a web-app inspired by daily fantasy games, such as DraftKings. DraftRoyale requires no real money to play. DraftRoyale is built on a Ruby and Rails backend, with a React/Redux cycle front end.</p>
       </div>
     );
--- frontend/flock.jsx
+++ frontend/flock.jsx
@@ -4,9 +4,12 @@ export default class Flock extends React.Component {
   render() {
     return(
       <div className='main-panel animated fadeIn'>
-        <video width='640' autoPlay loop>
-          <source src='./assets/flock.webm' type='video/webm'/>
-        </video>
+        <h2>Flocking Simulation</h2>
+        <a target='_blank' href='https://schan1031.github.io/Flocking-Simulation'>
+          <video width='640' autoPlay loop>
+            <source src='./assets/flock.webm' type='video/webm'/>
+          </video>
+        </a>
         <p>
           Flocking is a behavior exhibited by birds in groups in flight. This simulation shows the behavior of flocking, and allows you to tune parameters to see different behavior. The three main factors in flocking are cohesion, alignment and separation . The combination of these three factors forms the pattern we know as flocking.
         </p>
